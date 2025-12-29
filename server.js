a9dbb265340d080f0c81932730cbb81287b41f7e
Dharaneesh Mohanraj <dharaneeshmohanraj0001@gmail.com>
2025-12-29 20:20:23 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+const contactRoutes = require("./routes/contactRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+// connect database
+connectDB();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/api", contactRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend started successfully 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
